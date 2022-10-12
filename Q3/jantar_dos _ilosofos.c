44bd03e80b661dbb00be6166cd0d2883d8ce3757
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 17:25:57 -0300

    Comentários do código

--- Q3/jantar_dos _ilosofos.c
+++ Q3/jantar_dos _ilosofos.c
@@ -4,7 +4,7 @@
 #include<semaphore.h>
 #include<unistd.h>
 
-// Funções
+// Funções Filósofos e "comer"
 void* filos(void *);
 void* come(int);
 
@@ -23,39 +23,47 @@ int main()
 	// Inicializar semáforo
 	sem_init(&filosofos, 0, 4);
 	
-	for(i=0;i<5;i++){
+	// Loop para distrubuir garfos para os 5 filósofos
+	for(i = 0; i < 5 ; i++){
 		sem_init(&garfo[i], 0, 1);
 	}
-	for(i=0;i<5;i++){
+	
+	// Loop para criação de threads
+	for(i = 0; i < 5; i++){
 		a[i]=i;
 		pthread_create(&tid[i], NULL, filos, (void *)&a[i]);
 	}
-	for(i=0;i<5;i++){
+	// Loop para fazer juntar as threads
+	for(i = 0; i < 5; i++){
 		pthread_join(tid[i], NULL);
 	}
 }
-
+// A thread executa a função "Filos"
 void* filos(void * num)
 {
 	int fil = *(int *)num;
-
+	
+	// Aguarda caso não possua garfo
 	sem_wait(&filosofos);
 	sem_wait(&garfo[fil]);
 	sem_wait(&garfo[(fil+1)%5]);
 
+	// O filósofo, quando tem garfo, come
 	sleep(2);
 	come(fil);
-	printf("Filósofo %d terminou de comer\n", fil);
 
+	// Após comer, o próximo filósofo irá comer
+	printf("Filósofo %d terminou de comer\n", fil);
 	sem_post(&garfo[(fil+1)%5]);
 	sem_post(&garfo[fil]);
 	sem_post(&filosofos);
     
     return 0;
 }
-
+// A função "Filos" executa a função "come"
 void* come(int fil)
 {
+	// Indica qual filósofo está comendo
 	sleep(1);
 	printf("Filósofo %d está comendo\n",fil);
 	return 0;
