e074f2a4c30e69e4265979c1a8f1c5695218ec25
JD3an <89491859+JD3an@users.noreply.github.com>
2022-10-12 17:31:33 -0300

    Adição de comentários

--- Q4/Barbeiro_Dorminhoco.c
+++ Q4/Barbeiro_Dorminhoco.c
@@ -5,21 +5,20 @@
 #include <pthread.h>
 #include <semaphore.h>
 
-#define NUM_CLIENTES 5 // ** Pode ser trocado
+#define NUM_CLIENTES 5
 
-// Funções
+// Declaração de funções Cliente, Barbeiro e tempo aleatório
 void *cliente(void *num);
 void *barbeiro(void *barb);
 void taleatorio(int s); 
 
 /* Declarando semáforos */
-
 // Número de clientes esperando
 sem_t espera;
 // Cadeira
 sem_t cadeira;
 // Barbeiro pode dormir
-sem_t amimir;
+sem_t dormindo;
 // Cliente não pode se levantar enquanto corta o cabelo
 sem_t cortando;
 
@@ -39,50 +38,50 @@ int main(int argc, char *argv[]) {
 		exit(-1);
 	}
 
-	for (i=0; i<NUM_CLIENTES; i++) {
+	for (i = 0; i < NUM_CLIENTES; i++) {
 		Numero[i] = i;
 	}
 
 	// Inicializar os semáforos
 	sem_init(&espera, 0, cadeiras);
 	sem_init(&cadeira, 0, 1);
-	sem_init(&amimir, 0, 0);
+	sem_init(&dormindo, 0, 0);
 	sem_init(&cortando, 0, 0);
 
 	// Criação da thread do barbeiro
 	pthread_create(&barb, NULL, barbeiro, NULL);
 
 	// Criação da thread dos clientes
-	for (i=0; i<clientes; i++) {
+	for (i = 0; i < clientes; i++) {
 		pthread_create(&cli[i], NULL, cliente, (void *)&Numero[i]);
 	sleep(1);
 	}
 
-	for (i=0; i<clientes; i++) {
+	for (i = 0; i < clientes; i++) {
 		pthread_join(cli[i], NULL);
 		sleep(1);
 	}
 
 	terminado = 1;
-	sem_post(&amimir);
+	sem_post(&dormindo);
 	pthread_join(barb, NULL);
 }
 
 // Função cliente
 void *cliente(void *numero) {
 	int num = *(int *)numero;
-	printf("Cliente %d indo para barbearia\n", num);
+	printf("O cliente %d está indo para barbearia\n", num);
 	taleatorio(2);
-	printf("Cliente %d chegou à barbearia\n", num);
+	printf("O cliente %d chegou à barbearia\n", num);
 	sem_wait(&espera);
-	printf("Cliente %d entrou na sala de espera\n", num);
+	printf("O cliente %d entrou na sala de espera\n", num);
 	sem_wait(&cadeira);
 	sem_post(&espera);
-	printf("Cliente %d acordando o barbeiro\n", num);
-	sem_post(&amimir);
+	printf("O cliente %d está acordando o barbeiro\n", num);
+	sem_post(&dormindo);
 	sem_wait(&cortando);
 	sem_post(&cadeira);
-	printf("Cliente %d saindo da barbearia\n", num);
+	printf("O cliente %d está saindo da barbearia\n", num);
     return 0;
 }
 
@@ -90,7 +89,7 @@ void *cliente(void *numero) {
 void *barbeiro(void *barb) {
 	while (!terminado) {
 		printf("O barbeiro está dormindo\n");
-		sem_wait(&amimir);
+		sem_wait(&dormindo);
 		if (!terminado) {
 			printf("O barbeiro está cortando cabelo\n");
 			taleatorio(2);
@@ -98,7 +97,7 @@ void *barbeiro(void *barb) {
 			sem_post(&cortando);
 		}
 		else {
-			printf("O barbeiro irá para casa\n");
+			printf("O barbeiro vai para casa\n");
 		}
 	}
     return 0;
